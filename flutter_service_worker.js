7427501439bfe39fb8070d9fbe7b5c5d8d036174
Yuji HAMADA <yuji.tky.ykhm@gmail.com>
2025-08-30 11:05:10 +0900

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a549a7e752a0f6e86b39d4a2211e28b6
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "d8116e213104173691a56dff754ffffb",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "bdae5b78e47d954eb4495b593ab0e1d9",
+"flutter_bootstrap.js": "11fe29b2e5355e8afdd23bc59cbfd993",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
