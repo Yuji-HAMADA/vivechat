58f40858918a56ad8a762228498891038b483d37
Yuji-HAMADA <Yuji-HAMADA@users.noreply.github.com>
2025-08-31 12:00:44 +0000

    deploy: c79b58152dd66d525bb8bec43c92e6862c2dd3e2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3470876976"
+    serviceWorkerVersion: "2175343004"
   }
 });
