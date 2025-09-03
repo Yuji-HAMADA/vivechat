1b6584a06cc24c207057708e4c7bdb86bf71f547
Yuji HAMADA <yuji.tky.ykhm@gmail.com>
2025-09-03 22:05:53 +0900

    Update: set default characters

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,11 +1,8 @@
-import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:vivechat/home_screen.dart';
 import 'secrets.dart';
 import 'conversation_service.dart';
